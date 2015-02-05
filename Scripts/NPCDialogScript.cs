76c83db9336445671a4a97726fe2f90f7c84d440
Patrick Holmes <p_holmes1@u.pacific.edu>
2015-02-04 19:44:53 -0800

    Basic NPC Dialog Implemented
    Upon Entering a Trigger placed in front of Green and Yellow Boxes, a
    dialog is initiated.
    
    Format of NPC Dialog Skeleton:
    -NPC greets you, and asks a question
    -Any response causes Dialog box to close
    -If a negative response was selected, NPC will ask same question.
    Otherwise, NPC will respond with a departure message. Selecting the only
    response available will close the Dialog Box

---
+++ Scripts/NPCDialogScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCDialogScript : MonoBehaviour {
+
+	public string[] answerButtons;
+	public string[] Questions;
+	bool DisplayDialog = false;
+	bool ActivateQuest = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	void OnGUI()
+	{
+	  GUILayout.BeginArea(new Rect(620, 490, 400, 400));
+
+	  if(DisplayDialog && !ActivateQuest)
+	  {
+	    GUILayout.Label(Questions[0]);
+	    GUILayout.Label(Questions[1]);
+
+	    if (GUILayout.Button(answerButtons[0]))
+	    {
+	      ActivateQuest = true;
+		  DisplayDialog = false;
+	    }
+
+	    if (GUILayout.Button(answerButtons[1]))
+	      DisplayDialog = false;
+      }
+
+	  if (DisplayDialog && ActivateQuest)
+	  {
+	    GUILayout.Label(Questions[2]);
+
+		if (GUILayout.Button(answerButtons[2]))
+				DisplayDialog = false;
+	  }
+
+	  GUILayout.EndArea();
+	}
+
+	void OnTriggerEnter2D()
+	{
+      DisplayDialog = true;
+	}
+
+	void OnTriggerExit2D()
+	{
+	  DisplayDialog = false;
+	}
+}
