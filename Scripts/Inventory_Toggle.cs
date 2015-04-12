fa486f0577b4b1fda46bbc6ae88b530918c29889
Jeff Dwyer <Jeffreydwyer95@hotmail.com>
2015-04-12 15:06:38 -0700

    Button now toggles an inventory display CURRENT BUGS: Cannot click button after inventory is enabled

---
+++ Scripts/Inventory_Toggle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Inventory_Toggle : MonoBehaviour, IPointerClickHandler {
+
+	Button inventoryButton;
+	UnityEngine.Canvas inventory_Display;
+	UnityEngine.UI.Image testImage;
+
+	// Use this for initialization
+	void Start () {
+		inventoryButton = GameObject.Find("Inventory_Toggle").GetComponent<Button>();
+		inventory_Display = GameObject.Find("Inventory_Display").GetComponent<Canvas>();
+
+		inventory_Display.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnPointerClick (PointerEventData e){
+		var curState = inventory_Display.enabled;
+		inventory_Display.enabled = !curState;
+		Debug.Log("Button Clicked! Previous state was now " + curState.ToString());
+	}
+
+}
