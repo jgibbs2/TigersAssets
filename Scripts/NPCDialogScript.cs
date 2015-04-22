4b5a8a316963bc96d4c990886b3895ae59a044d8
Patrick Holmes <p_holmes1@u.pacific.edu>
2015-04-22 11:00:27 -0700

    NPC Dialog in Minotaur Tent ALMOST works
    Current "Bug": Can't get the first question to disappear upon response,
    but all subsequent responses are overridden upon response.

--- Scripts/NPCDialogScript.cs
+++ Scripts/NPCDialogScript.cs
@@ -4,55 +4,72 @@ using System.Collections;
 public class NPCDialogScript : MonoBehaviour
 {
 
-	public string answerButton;
+	public string[] answerButtons;
 	public string[] Questions;
-	bool DisplayDialog = false;
+	public bool[] DisplayDialog;
 	bool ActivateQuest = false;
 	public bool inTrigger = false;
 
 	// Use this for initialization
 	void Start () 
 	{
+	  for (int i = 0; i < DisplayDialog.Length; i++)
+			DisplayDialog[i] = false;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 	  if (inTrigger && (GameObject.Find("Bobby").GetComponent<SpriteController>().xButtonPressed || Input.GetKeyDown(KeyCode.Space))) 
-	    DisplayDialog = true;
+	  {
+	    if (!ActivateQuest)
+	      DisplayDialog[0] = true;
+
+		else
+		  DisplayDialog[DisplayDialog.Length - 1] = true;
+	  }
 	}
 
 	void OnGUI()
 	{
 	  GUILayout.BeginArea(new Rect(310, 0, 400, 400));
 
-	  if(DisplayDialog && !ActivateQuest)
+      for (int i = 0; i < Questions.Length - 1; i++)
 	  {
-		for (int i = 0; i < Questions.Length - 1; i++)
-		{
+	    if(DisplayDialog[i] && !ActivateQuest)
+	    {
 	      GUILayout.Label(Questions[i]);
-
-	      if (GUILayout.Button(answerButton))
-		  {
-		    if (i == Questions.Length - 2)
+		
+		  if (GUILayout.Button(answerButtons[i]))
+	      {
+			if (i == Questions.Length - 2)
 			{
-	          ActivateQuest = true;
-		      DisplayDialog = false;
+			  DisplayDialog[i] = false;
+			  ActivateQuest = true;
+			  GameObject.Find("Bobby").GetComponent<SpriteController>().xButtonPressed = false;
 			}
-	      }
-		}
 
-	    /*if (GUILayout.Button(answerButtons[1]))
-	      DisplayDialog = false;*/
-      }
-
-	  if (DisplayDialog && ActivateQuest)
-	  {
-	    GUILayout.Label(Questions[Questions.Length - 1]);
-
-		if (GUILayout.Button(answerButton))
-		  DisplayDialog = false;
+			else
+			{
+			  DisplayDialog[i]     = false;
+			  DisplayDialog[i + 1] = true;
+			}
+		  } 
+		}	  
 	  }
+
+	    if (DisplayDialog[DisplayDialog.Length - 1] && ActivateQuest)
+	    {
+	      GUILayout.Label(Questions[Questions.Length - 1]);
+
+		  if (GUILayout.Button(answerButtons[answerButtons.Length - 1]))
+		  {
+		    for (int i = 0; i < DisplayDialog.Length; i++)
+			  DisplayDialog[i] = false;
+
+			ActivateQuest = false;
+		  }
+	    }
 
 	  GUILayout.EndArea();
 	}
@@ -65,7 +82,7 @@ public class NPCDialogScript : MonoBehaviour
 	void OnTriggerExit2D(Collider2D col)
 	{
 		inTrigger = false;
-		DisplayDialog = false;
+		//DisplayDialog = false; 
 	    GameObject.Find("Bobby").GetComponent<SpriteController>().xButtonPressed = false;
 	}
 }
