20cb2e3cce528dd91623505bff5b4bb3efef897e
jgibbs2 <j_gibbs2>
2015-04-30 18:38:18 -0700

    only pick up each character once!
    changed data in jeff's file to only allow our party members to only
    appear and get picked up once.

--- Scripts/OnTentEntranceScript.cs
+++ Scripts/OnTentEntranceScript.cs
@@ -9,29 +9,35 @@ public class OnTentEntranceScript : MonoBehaviour
 {
 	private bool hasVisitedTrigger = false;
 	public string name_of_party_member;
+	private bool secondTimeInTent;
 
 	// Use this for initialization
 	void Start () 
 	{
-		GetComponent<SpriteController>().player_controlled = false;
+		if(GameObject.Find("GameData").GetComponent<GameData>().characters[GameObject.Find("Sprite PC").GetComponent<PCDialogScript>().num] == true)
+		{
+			secondTimeInTent = true;
+			Destroy(GameObject.Find("Sprite PC"));
+		}
+		else{
+			GetComponent<SpriteController>().player_controlled = false;
+		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (!hasVisitedTrigger && GetComponent<SpriteController>().player_controlled == false)
-	  {
-	    if (!GameObject.Find(name_of_party_member).GetComponentInChildren<PCDialogScript>().inClanMemberTrigger) 
-	    {
-		  transform.Translate(2.5f * Vector2.up * Time.deltaTime);
-		}
-
-		else
-		{
-		  hasVisitedTrigger = true;
-		  GameObject.Find("Bobby").GetComponent<SpriteController>().xButtonPressed = true;
-		  GameObject.Find("Bobby").GetComponent<SpriteController>().player_controlled = true;
-	    }
-	  }
+		if (secondTimeInTent == false) {
+						if (!hasVisitedTrigger && GetComponent<SpriteController> ().player_controlled == false) {
+								if (!GameObject.Find (name_of_party_member).GetComponentInChildren<PCDialogScript> ().inClanMemberTrigger) {
+										transform.Translate (2.5f * Vector2.up * Time.deltaTime);
+								} else {
+										hasVisitedTrigger = true;
+										GameObject.Find ("Bobby").GetComponent<SpriteController> ().xButtonPressed = true;
+										GameObject.Find ("Bobby").GetComponent<SpriteController> ().player_controlled = true;
+								}
+						}
+				}
 	}
 }
