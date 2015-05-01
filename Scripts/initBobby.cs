26c5b1a10cd9c38ce3532825ead1261562b4ae47
jgibbs2 <j_gibbs2>
2015-04-30 21:31:05 -0700

    Green and Blue Tents
    -Green tent and blue tent are populated
    -adding art and creatures for the other ones
    -Each tent needs a quest-giver

--- Scripts/initBobby.cs
+++ Scripts/initBobby.cs
@@ -7,7 +7,7 @@ public class initBobby : MonoBehaviour {
 	{
 		Debug.Log (GameData.access.getBobbyX_parade ());
 		Debug.Log (GameData.access.getBobbyY_parade ());
-		var player = GameObject.FindWithTag ("Player");
+		var player = GameObject.Find ("Bobby");
 		if(player != null)
 			player.transform.position = new Vector2(GameData.access.getBobbyX_parade(),GameData.access.getBobbyY_parade()-0.25f);
 		else
