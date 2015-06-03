2dc72a86144515f56246b32d1a136b8793d3df46
Patrick Holmes <p_holmes1@u.pacific.edu>
2015-06-03 16:18:38 -0700

    Testing Text revamp
    Clicking Red Text will trigger an event.
    If this is satisfactory, it can be scaled and used in the existing NPC
    Dialog Script.

--- Scripts/OneLiner.cs
+++ Scripts/OneLiner.cs
@@ -1,42 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OneLiner : MonoBehaviour {
+public class OneLiner : MonoBehaviour 
+{
 	private GUIStyle myStyle;
+	private GUIStyle yourStyle;
 	public Texture2D texture;
 	public Texture2D characterImage;
 
 	private bool speech = false;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		myStyle = new GUIStyle();
 		myStyle.fontSize = 30;
 		myStyle.normal.textColor = Color.white;
 		myStyle.fontStyle = FontStyle.Bold;
 		myStyle.normal.background = texture;
+
+		yourStyle = new GUIStyle();
+		yourStyle.fontSize = 30;
+		yourStyle.normal.textColor = Color.red;
+		//yourStyle.fontStyle = FontStyle.Bold;  
+		yourStyle.normal.background = texture;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	// Update is called once per frame 
+	void Update () 
+	{
 		if(Input.GetKeyDown(KeyCode.Space))
-		{
-			if(speech)
-			{
-				speech = false;
-			}
-			else
-			{
-				speech = true;
-			}
-		}
+			speech = !speech;
 	}
 
-	void OnGUI(){
+	void OnGUI()
+	{
 		if(speech)
 		{
 			GUI.DrawTexture(new Rect(450, 100, 300, 300), characterImage);
 			GUI.Label(new Rect(0,350,Screen.width,Screen.height),"Bobby: All your base are belong to us!", myStyle);
 
+			if (GUI.Button(new Rect(0,385,Screen.width,Screen.height),"Yo Mama: Not if I got anything to do wid it!", yourStyle))
+			{
+			  Debug.Log("Okay, fine then punk!");
+			}
 		}
 
 	}
